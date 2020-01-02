1393babf962b8cd8f120e75730112e1501c50eef
lowapple <lowapple99@gmail.com>
2020-01-02 14:41:35 +0900

    상단 바 데이터 고정

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,18 +3,31 @@ import Helmet from 'react-helmet'
 import Header from '../components/Header'
 import { StaticQuery, graphql } from 'gatsby'
 
-const Template = ({ title, children }) => {
+const Template = ({ children }) => {
     return (
+        <StaticQuery
+        query={graphql`
+        query Header {
+            site {
+            siteMetadata {
+              title
+              description
+            }
+          }
+        }
+        `}
+        render={sitemap => (
         <>
             <Helmet>
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Helmet>
-            <Header title={title}/>
+            <Header title={sitemap.site.siteMetadata.title}/>
             <div id="content">
                 {children}
             </div>
         </>
+        )}/>
     )
 }
 
--- src/templates/index.js
+++ src/templates/index.js
@@ -25,53 +25,13 @@ const IndexPage = ({ pageContext }) => {
   ))
 
   return (
-    <StaticQuery
-        query={graphql`
-        query Header {
-            site {
-            siteMetadata {
-              title
-              description
-            }
-          }
-        }
-        `}
-        render={sitemap => (
-        <Layout title={sitemap.site.siteMetadata.title}>
-          <SEO title="Home"/>
-          <PageHeader 
-            title={sitemap.site.siteMetadata.title} 
-            description={sitemap.site.siteMetadata.description}/>
-          <section>
-            <ul>
-              {postsContent(posts)}
-            </ul>
-          </section>
-          <Pagenation>
-            <ReactPaginate
-              previousLabel={<i className="fas fa-angle-left" />}
-              nextLabel={<i className="fas fa-angle-right" />}
-              breakLabel={<i className="fa fa-ellipsis-h" />}
-              forcePage={pageContext.index - 1}
-              breakClassName={'break-me'}
-              pageCount={pageContext.pageCount}
-              marginPagesDisplayed={2}
-              pageRangeDisplayed={5}
-              onPageChange={handlePageClick}
-              containerClassName={'pagination'}
-              subContainerClassName={'pages pagination'}
-              activeClassName={'active'}
-            />
-          </Pagenation>
-        </Layout>
-        )}
-    />
-  )
-
-  return (
-  <Layout>
+    <Layout>
     <SEO title="Home"/>
-    <PageHeader title={"로우애플 기술 블로그"} description={"기타등등 이것저것 여러가지"}/>
+    <section>
+      <ul>
+        {postsContent(posts)}
+      </ul>
+    </section>
     <Pagenation>
       <ReactPaginate
         previousLabel={<i className="fas fa-angle-left" />}
@@ -93,61 +53,3 @@ const IndexPage = ({ pageContext }) => {
 }
 
 export default IndexPage
-
-/*
-const IndexPage = ({ data }) => {
-    // 포스트 리스트 필터
-    const posts = data.allMarkdownRemark.edges.filter(
-      p => p.node.frontmatter.date !== null && p.node.frontmatter.type === "archive"
-    )
-    // PostElement 리스트 생성 
-    const postsList = posts =>
-      posts.map(post => (
-        <PostElement post={post} key={post.node.id} />
-      ))
-
-    // PostElement 리스트 생성 with 연도 그룹
-    const postsListContainer = groupBy(posts, getDateYear)
-      .map(({ year, posts }, i) => (
-        <div key={i}>
-          <p><b>{year}</b> 년 포스트</p>
-          {postsList(posts)}
-        </div>
-      )).reverse()
-
-    // 렌터링
-    return (
-      <DefaultLayout>
-        <SEO title="Home" />
-        <PageHeader title={"로우애플 기술 블로그"} description={"기타등등 이것저것 여러가지"}/>
-        <section>
-          <ul>{postsListContainer}</ul>
-        </section>
-      </DefaultLayout>
-    )
-}
-
-export default IndexPage
-
-export const pageQuery = graphql`
-  query {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-      edges {
-        node {
-          id
-          fields {
-            slug
-          }
-          frontmatter {
-            date
-            title
-            tags
-            description
-            type
-          }
-        }
-      }
-    }
-  }
-`
-*/
