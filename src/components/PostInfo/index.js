0ea096780b6912ff4e3a9b7951d822e597c12067
lowapple <lowapple99@gmail.com>
2020-01-06 16:43:55 +0900

    포스트 timeToRead 수정

--- src/components/PostInfo/index.js
+++ src/components/PostInfo/index.js
@@ -3,10 +3,10 @@ import classNames from 'classnames'
 import styles from './style.module.scss'
 import PropTypes from "prop-types"
 
-const PostInfo = ({ date, readTime }) => { 
+const PostInfo = ({ date, timeToRead }) => { 
     return (
     <div className={classNames(styles['default'])}>
-        {date} &middot; {readTime} min to read
+        {date} &middot; {timeToRead} min to read
     </div>
     )
 }
