87a426fbee91285d980fcb9669d74dd49fe7985f
lowapple <lowapple99@gmail.com>
2020-01-06 16:35:25 +0900

    스타일 변경

--- src/components/Head.js
+++ src/components/Head.js
@@ -7,6 +7,7 @@ export default function Head({ children }) {
       <Helmet>
           <link href='//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-kr.css' rel='stylesheet' type='text/css'/>
           <link href='//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-jp.css' rel='stylesheet' type='text/css'/>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
           {children}
       </Helmet>
     </div>
